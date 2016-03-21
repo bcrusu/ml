d508b6f87c3ae8a155e8aeb7b1c91408c9936ccd
bcrusu <bcrusu@users.noreply.github.com>
2016-03-21 16:36:05 +0200

    working on KNN classifier

--- cs231n/assignment1/cs231n/data_utils.py
+++ cs231n/assignment1/cs231n/data_utils.py
@@ -19,14 +19,16 @@ def load_CIFAR10(ROOT):
     """ load all of cifar """
     xs = []
     ys = []
-    for b in range(1, 6):
+    for b in range(1, 4):  # TODO: load all data files
         f = os.path.join(ROOT, 'data_batch_%d' % (b,))
         X, Y = load_CIFAR_batch(f)
         xs.append(X)
         ys.append(Y)
     Xtr = np.concatenate(xs)
+    del xs
     Ytr = np.concatenate(ys)
-    del X, Y
+    del ys
+
     Xte, Yte = load_CIFAR_batch(os.path.join(ROOT, 'test_batch'))
     return Xtr, Ytr, Xte, Yte
 
--- cs231n/assignment1/knn.py
+++ cs231n/assignment1/knn.py
@@ -1,17 +1,70 @@
-import random
 import numpy as np
 from cs231n.data_utils import load_CIFAR10
 import matplotlib.pyplot as plt
+from cs231n.classifiers import KNearestNeighbor
 
+# Global settings
+plt.interactive(False)
 
 # Load the raw CIFAR-10 data.
-cifar10_dir = './cs231n/datasets/cifar-10-batches-py'
-X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+X_train, y_train, X_test, y_test = load_CIFAR10('./cs231n/datasets/cifar-10-batches-py')
 
-# As a sanity check, we print out the size of the training and test data.
-print('Training data shape: ', X_train.shape)
-print('Training labels shape: ', y_train.shape)
-print('Test data shape: ', X_test.shape)
-print('Test labels shape: ', y_test.shape)
+# Subsample the data for more efficient code execution in this exercise
+num_training = 5000
+mask = range(num_training)
+X_train = X_train[mask]
+y_train = y_train[mask]
 
-#TODO...
+num_test = 500
+mask = range(num_test)
+X_test = X_test[mask]
+y_test = y_test[mask]
+
+# Reshape the image data into rows
+X_train = np.reshape(X_train, (X_train.shape[0], -1))
+X_test = np.reshape(X_test, (X_test.shape[0], -1))
+# print(X_train.shape, X_test.shape)
+
+
+def visualize_random():
+    # Visualize some examples from the dataset.
+    # We show a few examples of training images from each class.
+    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    num_classes = len(classes)
+    samples_per_class = 7
+    for y, cls in enumerate(classes):
+        idxs = np.flatnonzero(y_train == y)
+        idxs = np.random.choice(idxs, samples_per_class, replace=False)
+        for i, idx in enumerate(idxs):
+            plt_idx = i * num_classes + y + 1
+            plt.subplot(samples_per_class, num_classes, plt_idx)
+            plt.imshow(X_train[idx].astype('uint8'))
+            plt.axis('off')
+            if i == 0:
+                plt.title(cls)
+    plt.show()
+
+
+classifier = KNearestNeighbor()
+classifier.train(X_train, y_train)
+
+dists = classifier.compute_distances_one_loop(X_test)
+print(dists.shape)
+plt.imshow(dists, interpolation='none')
+plt.show()
+
+exit(-999)
+
+y_test_pred = classifier.predict_labels(dists, k=1)
+
+# Compute and print the fraction of correctly predicted examples
+# k=1
+num_correct = np.sum(y_test_pred == y_test)
+accuracy = float(num_correct) / num_test
+print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
+
+# k=5
+y_test_pred = classifier.predict_labels(dists, k=5)
+num_correct = np.sum(y_test_pred == y_test)
+accuracy = float(num_correct) / num_test
+print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
