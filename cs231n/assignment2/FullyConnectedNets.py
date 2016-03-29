038d771bef6759c77b61b071dc27a005081fa6e6
bcrusu <bcrusu@users.noreply.github.com>
2016-03-29 09:42:48 +0300

    sgd vs. sgd_momentum

--- cs231n/assignment2/FullyConnectedNets.py
+++ cs231n/assignment2/FullyConnectedNets.py
@@ -273,4 +273,116 @@ def test_FullyConnectedNet_gradient():
             print('%s relative error: %.2e' % (name, rel_error(grad_num, grads[name])))
 
 
-test_FullyConnectedNet_gradient()
+def test_FullyConnectedNet_overfit(num_layers, learning_rate):
+    num_train = 50
+    small_data = {
+        'X_train': data['X_train'][:num_train],
+        'y_train': data['y_train'][:num_train],
+        'X_val': data['X_val'],
+        'y_val': data['y_val'],
+    }
+
+    weight_scale = 3e-2
+    hidden_dims = np.full(num_layers - 1, 100, np.int32)
+    model = FullyConnectedNet(hidden_dims, weight_scale=weight_scale, dtype=np.float64)
+    solver = Solver(model, small_data,
+                    print_every=10, num_epochs=20, batch_size=25,
+                    update_rule='sgd',
+                    optim_config={
+                        'learning_rate': learning_rate
+                    })
+    solver.train()
+
+    plt.plot(solver.loss_history, 'o')
+    plt.title('Training loss history')
+    plt.xlabel('Iteration')
+    plt.ylabel('Training loss')
+    plt.show()
+
+
+# overfit 3 and 5 layer nets
+# test_FullyConnectedNet_overfit(3, 8e-4)
+# test_FullyConnectedNet_overfit(5, 39e-3)
+
+
+def test_sgd_momentum():
+    from cs231n.optim import sgd_momentum
+
+    N, D = 4, 5
+    w = np.linspace(-0.4, 0.6, num=N * D).reshape(N, D)
+    dw = np.linspace(-0.6, 0.4, num=N * D).reshape(N, D)
+    v = np.linspace(0.6, 0.9, num=N * D).reshape(N, D)
+
+    config = {'learning_rate': 1e-3, 'velocity': v}
+    next_w, _ = sgd_momentum(w, dw, config=config)
+
+    expected_next_w = np.asarray([
+        [0.1406, 0.20738947, 0.27417895, 0.34096842, 0.40775789],
+        [0.47454737, 0.54133684, 0.60812632, 0.67491579, 0.74170526],
+        [0.80849474, 0.87528421, 0.94207368, 1.00886316, 1.07565263],
+        [1.14244211, 1.20923158, 1.27602105, 1.34281053, 1.4096]])
+    expected_velocity = np.asarray([
+        [0.5406, 0.55475789, 0.56891579, 0.58307368, 0.59723158],
+        [0.61138947, 0.62554737, 0.63970526, 0.65386316, 0.66802105],
+        [0.68217895, 0.69633684, 0.71049474, 0.72465263, 0.73881053],
+        [0.75296842, 0.76712632, 0.78128421, 0.79544211, 0.8096]])
+
+    print('next_w error: ', rel_error(next_w, expected_next_w))
+    print('velocity error: ', rel_error(expected_velocity, config['velocity']))
+
+
+def test_sgd_vs_sgd_momentum():
+    num_train = 4000
+    small_data = {
+        'X_train': data['X_train'][:num_train],
+        'y_train': data['y_train'][:num_train],
+        'X_val': data['X_val'],
+        'y_val': data['y_val'],
+    }
+
+    solvers = {}
+    for update_rule in ['sgd', 'sgd_momentum']:
+        print('running with ', update_rule)
+        model = FullyConnectedNet([100, 100, 100, 100, 100], weight_scale=5e-2)
+
+        solver = Solver(model, small_data,
+                        num_epochs=5, batch_size=100,
+                        update_rule=update_rule,
+                        optim_config={
+                            'learning_rate': 1e-2
+                        },
+                        verbose=True)
+        solvers[update_rule] = solver
+        solver.train()
+        print('-----------------------------------------------------------------')
+
+    plt.subplot(3, 1, 1)
+    plt.title('Training loss')
+    plt.xlabel('Iteration')
+
+    plt.subplot(3, 1, 2)
+    plt.title('Training accuracy')
+    plt.xlabel('Epoch')
+
+    plt.subplot(3, 1, 3)
+    plt.title('Validation accuracy')
+    plt.xlabel('Epoch')
+
+    for update_rule, solver in solvers.items():
+        plt.subplot(3, 1, 1)
+        plt.plot(solver.loss_history, 'o', label=update_rule)
+
+        plt.subplot(3, 1, 2)
+        plt.plot(solver.train_acc_history, '-o', label=update_rule)
+
+        plt.subplot(3, 1, 3)
+        plt.plot(solver.val_acc_history, '-o', label=update_rule)
+
+    for i in [1, 2, 3]:
+        plt.subplot(3, 1, i)
+        plt.legend(loc='upper center', ncol=4)
+    plt.gcf().set_size_inches(15, 15)
+    plt.show()
+
+
+test_sgd_vs_sgd_momentum()
