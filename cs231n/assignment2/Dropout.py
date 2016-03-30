3e942ba0a4a2c02fd73a53327a85d6ff054e5075
bcrusu <bcrusu@users.noreply.github.com>
2016-03-30 03:23:50 +0300

    dropout vs no dropout test

--- cs231n/assignment2/Dropout.py
+++ cs231n/assignment2/Dropout.py
@@ -64,4 +64,58 @@ def test_FullyConnectedNet_with_dropout():
             print('%s relative error: %.2e' % (name, rel_error(grad_num, grads[name])))
 
 
-test_FullyConnectedNet_with_dropout()
+def test_FullyConnectedNet_with_dropout_vs_without_dropout():
+    # Train two identical nets, one with dropout and one without
+
+    num_train = 500
+    small_data = {
+        'X_train': data['X_train'][:num_train],
+        'y_train': data['y_train'][:num_train],
+        'X_val': data['X_val'],
+        'y_val': data['y_val'],
+    }
+
+    solvers = {}
+    dropout_choices = [0, 0.75]
+    for dropout in dropout_choices:
+        model = FullyConnectedNet([500], dropout=dropout)
+
+        solver = Solver(model, small_data,
+                        num_epochs=25, batch_size=100,
+                        update_rule='adam',
+                        optim_config={
+                            'learning_rate': 5e-4,
+                        },
+                        verbose=True, print_every=100)
+        solver.train()
+        solvers[dropout] = solver
+
+    # Plot train and validation accuracies of the two models
+    train_accs = []
+    val_accs = []
+    for dropout in dropout_choices:
+        solver = solvers[dropout]
+        train_accs.append(solver.train_acc_history[-1])
+        val_accs.append(solver.val_acc_history[-1])
+
+    plt.subplot(3, 1, 1)
+    for dropout in dropout_choices:
+        plt.plot(solvers[dropout].train_acc_history, 'o', label='%.2f dropout' % dropout)
+    plt.title('Train accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(ncol=2, loc='lower right')
+
+    plt.subplot(3, 1, 2)
+    for dropout in dropout_choices:
+        plt.plot(solvers[dropout].val_acc_history, 'o', label='%.2f dropout' % dropout)
+    plt.title('Val accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(ncol=2, loc='lower right')
+
+    plt.gcf().set_size_inches(15, 15)
+    plt.show()
+
+
+test_FullyConnectedNet_with_dropout_vs_without_dropout()
